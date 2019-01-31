715bd1e1def2e7c84ffd934abb759b43d16dc3df
Lucas Alexandre Occaso <39532512+lucasoccaso@users.noreply.github.com>
2019-01-31 15:06:54 -0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -8,23 +8,42 @@
 
 '''
 
-import time
-import os
-from flask import Flask, request, redirect, url_for, current_app, send_from_directory
-from werkzeug.utils import secure_filename
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from bson import json_util
+from pprint import pprint
+import json
+import bson
 
 
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = "mongodb://admin:admin123@ds117545.mlab.com:17545/db_cloudbroker"
+mongo = PyMongo(app)
 
 
 @app.route('/Divulga', methods=['GET', 'POST'])
 def Divulga():
-    
+    data = request.get_json()
+    print (data)
+    mongo.db['provedores'].insert_one(
+        {
+            'provedor_id': data['provedor_id'],
+            'vcpu': data['vcpu'],
+            'ram': data['ram'],
+            'disco': data['disco'],
+            'custo': data['custo'],  
+            'disponibilidade': 'True',
+        })
     return 'Dados do provedor divulgados'
 
 
 @app.route('/Consulta', methods=['GET', 'POST'])
 def Consulta():
+    with open('db.txt', 'r') as arquivo:
+        json_data = arquivo.read() 
+    arquivo.close()
+
+    print (json_data)
 
     return '1'
